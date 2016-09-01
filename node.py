143327a528819c206e4afc2ad3a638616589ed1e
root <root@test-22.at.office>
2016-09-01 18:31:02 +0200

    rm Process.

--- node.py
+++ node.py
@@ -4,133 +4,99 @@ from multiprocessing import Process
 from multiprocessing.managers import BaseManager
 import multiprocessing, Queue, time, sys, random, pprint, signal, uuid
 
-HOST = '10.4.4.22'  
-PORT = 50000  
+HOST = '10.4.4.22'
+QPORT = 50000
+SPORT = 50001
 AUTH_KEY = 'a secret'
 
-class QueueManager(BaseManager): pass 
+taksq, resq = None, None
 
-class QueueProc(Process):  
-    def __init__(self, *args, **kwargs):  
-        self.taskq = Queue.Queue()
-        super(QueueProc, self).__init__(*args, **kwargs)
-    def run(self):  
-        #rpc
-        QueueManager.register('get_taskq', callable = lambda:self.taskq)  
-        #listen
-        manager = QueueManager(address = (HOST,PORT), authkey = AUTH_KEY)  
-        server = manager.get_server()  
-        try:
-            server.serve_forever()  
-        except BaseException,e:
-            print('%s caught exception:%r' % (self.name, e))
+def reg_rpc():
+    BaseManager.register('get_taskq', callable = lambda:taskq)  
+    BaseManager.register('get_resq', callable = lambda:resq)  
+
+def start_queue():
+    global taskq, resq
+    taskq, resq = Queue.Queue(), Queue.Queue()
+    reg_rpc()
+    print('listen at %s:%s ..' % (HOST,QPORT))
+    mgr = BaseManager(address = (HOST,QPORT), authkey = AUTH_KEY)  
+    #print('before mgr.start() %s' % multiprocessing.current_process().pid)
+    mgr.get_server().serve_forever() #manager run in current proc
+    '''
+    mgr.start() #start child process to run manager
+    print('after mgr.start() %s' % multiprocessing.current_process().pid)
+    proxyq = mgr.get_taskq()
+    while True:
+        print('after mgr.start() %s queue sz %s, proxy q sz %s' % (multiprocessing.current_process().pid, taskq.qsize(), proxyq.qsize()))
+        time.sleep(5)
+    '''
   
-class Worker(Process):  
-    def run(self):  
-        #rpc
-        QueueManager.register('get_taskq')  
-        #connect
-        manager = QueueManager(address = (HOST,PORT), authkey = AUTH_KEY)  
-        manager.connect()  
-        #fetch task
-        self.taskq = manager.get_taskq()  
-        try:
-            while True:
-                task = self.taskq.get()  
-                print '%s(%s) get task "%s", %s left in queue' % (self.name, self.pid, task, self.taskq.qsize())  
-        except BaseException,e:
-            print('%s caught exception:%r' % (self.name, e))
-        
-class Scheduler(Process):  
-    def run(self):  
-        #rpc
-        QueueManager.register('get_taskq')
-        #connect
-        manager = QueueManager(address = (HOST,PORT), authkey = AUTH_KEY)  
-        manager.connect()  
-        #push task
-        self.taskq = manager.get_taskq()  
-        try:
-            while True:  
-                tid = uuid.uuid1()
-                task = {'id':tid, 'data':{}} 
-                self.taskq.put(task)  
-                print '%s(%s) put task "%s", %s left in queue' % (self.name, self.pid, task, self.taskq.qsize())  
-                #time.sleep(random.randrange(5))
-        except BaseException,e:
-            print('%s caught exception:%r' % (self.name, e))
+def start_scheduler():
+    reg_rpc()
+    #mgr = BaseManager(address = (HOST,SPORT), authkey = AUTH_KEY)
+    #mgr.start()
+    qmgr = BaseManager(address = (HOST,QPORT), authkey = AUTH_KEY)  
+    qmgr.connect()
+    taskq = qmgr.get_taskq()
+    while True:
+        tid = uuid.uuid1()
+        task = {'id':tid, 'data':{}}
+        taskq.put(task)
+        print 'put task "%s", %s left in queue' % (task, taskq.qsize())
+        time.sleep(random.randrange(5))
+
+def start_worker():
+    reg_rpc()
+    qmgr = BaseManager(address = (HOST,QPORT), authkey = AUTH_KEY)
+    qmgr.connect()
+    taskq = qmgr.get_taskq()
+    while True:
+        task = taskq.get()  
+        print 'get task "%s", %s left in queue' % (task, taskq.qsize())  
 
 def save_pid(pname = None):
     p = multiprocessing.current_process()
     #print('save_pid(%s) p.name %s, p.pid %s' % (pname, p.name, p.pid))
     file('%s.pid' % (pname or p.name,), 'w').write(str(p.pid))
 
+def daemon_start(func):
+    p = None
+    def over():
+        p.terminate()
+        p.join()
+        sys.exit(0)
+    def sighandler(signum, frame):  
+        print('sighandler %s %s' % (signum, frame))
+        over()
+    def set_signal():  
+        signal.signal(signal.SIGTERM, sighandler)  
+        signal.signal(signal.SIGINT, sighandler)  
+    def clear_signal():  
+        signal.signal(signal.SIGTERM, 0)  
+        signal.signal(signal.SIGINT, 0) 
+    try:
+        while True:
+            clear_signal()
+            p = Process(target=func)
+            p.daemon = True
+            p.start()
+            set_signal()
+            p.join()
+    except BaseException,e:
+        over()
+
 if __name__ == '__main__':  
     if len(sys.argv) < 2:
-        print('invalid argument.')
-        sys.exit(0)
+        print('invalid argument.'); sys.exit(0)
+    #pprint.pprint(sys.argv)
     arg1 = sys.argv[1]
+    if arg1 == 'queue':
+        start_queue()
     if arg1 == 'scheduler':
-        #queue proc
-        queueProc = QueueProc(name='queueproc')
-        queueProc.daemon = True
-        queueProc.start()  
-        #sched proc
-        scheduler = Scheduler(name='schedproc')
-        scheduler.daemon = True  
-        scheduler.start()
-        #main proc
-        def over():
-            queueProc.terminate()
-            queueProc.join()
-            scheduler.terminate()
-            scheduler.join()
-            sys.exit(0)
-        def sighandler(signum, frame):  
-            print("proc %s terminate signal received: sig(%s)" % (multiprocessing.current_process().name, signum))  
-            over()
-        save_pid('schedmain')
-        signal.signal(signal.SIGTERM, sighandler)  
-        signal.signal(signal.SIGINT, sighandler)
-        try:
-            while True:
-                time.sleep(1)
-        except BaseException,e:
-            print('main proc caught exception: %r' % e)
-            over()
+        daemon_start(start_scheduler)
     elif arg1 == 'worker':
-        #worker
-        num = len(sys.argv)>2 and int(sys.argv[2]) or 5
-        workers = [Worker(name='worker%s'%i) for i in range(num)]  
-        for worker in workers:  
-            worker.daemon = True  
-            worker.start()
-        #main proc
-        def over():
-            for worker in workers:
-                worker.terminate()
-                worker.join()
-            sys.exit(0)
-        def sighandler(signum, frame):  
-            print("proc %s terminate signal received: sig(%s)" % (multiprocessing.current_process().name, signum))  
-            over()
-        save_pid('workermain')
-        signal.signal(signal.SIGTERM, sighandler)  
-        signal.signal(signal.SIGINT, sighandler)
-        try:
-            while True:
-                for index in range(len(workers)):
-                    worker = workers[index]
-                    if not worker.is_alive():
-                        name = worker.name
-                        print('child no alive any more:%s %s' % (name,worker))
-                        worker.terminate()
-                        worker.join()
-                        worker = Worker(name=name)
-                        worker.daemon = True
-                        worker.start()
-                        workers[index] = worker
-                time.sleep(1)
-        except BaseException,e:
-            print('main proc caught exception: %r' % e)
-            over()
+        daemon_start(start_worker)
+    else:
+        #print('cur process %s' % multiprocessing.current_process().pid)
+        assert(False)
